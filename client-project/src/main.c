3b5479df3866002a6138704dc94603cd42e88dae
danielegenchi-afk <danielegenchi09@gmail.com>
2025-11-25 11:16:04 +0100

    Add files via upload

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -21,52 +21,173 @@
 #endif
 
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include "protocol.h"
 
+#include "protocol.h"
 #define NO_ERROR 0
 
 void clearwinsock() {
 #if defined WIN32
-	WSACleanup();
+    WSACleanup();
 #endif
 }
 
+void errorhandler(char *msg) {
+    printf("%s\n", msg);
+}
+
 int main(int argc, char *argv[]) {
+	//controllo parametri di input
+	char server_ip[16]="127.0.0.1";
+	    int server_port = SERVER_PORT;
+	    char *request_str = NULL;
+	for (int i = 1; i < argc; i++) {
+	        if (strcmp(argv[i], "-s") == 0) {
+	            if (i + 1 < argc) {
+	                strncpy(server_ip, argv[i+1], sizeof(server_ip) - 1);
+	                server_ip[sizeof(server_ip) - 1] = '\0';
+	                i++;
+	            }
+	        } else if (strcmp(argv[i], "-p") == 0) {
+	            if (i + 1 < argc) {
+	                server_port = atoi(argv[i+1]);
+	                i++;
+	            }
+	        } else if (strcmp(argv[i], "-r") == 0) {
+	            if (i + 1 < argc) {
+	                request_str = argv[i+1];
+	                i++;
+	            }
+	        }
+	    }
 
-	// TODO: Implement client logic
+	    if (request_str == NULL) {
+	        printf("Errore: Il parametro '-r request' è obbligatorio.\n");
+	        printf("Utilizzo: ./client-project [-s server] [-p port] -r \"tipo città\"\n");
+	        return 1;
+	    }
+	    if (strlen(request_str) < 2) {
+	        printf("Errore: La richiesta deve contenere almeno tipo e città.\n");
+	        return 1;
+	    }
 
-#if defined WIN32
-	// Initialize Winsock
-	WSADATA wsa_data;
-	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
-	if (result != NO_ERROR) {
-		printf("Error at WSAStartup()\n");
-		return 0;
-	}
-#endif
+	    //creazione richiesta
+    weather_request_t req;
+    memset(&req, 0, sizeof(req));
 
-	int my_socket;
+    req.type = request_str[0];
 
-	// TODO: Create socket
-	// my_socket = socket(...);
+        int start_index = 1;
+        while (request_str[start_index] == ' ') {
+            start_index++;
+        }
+        snprintf(req.city, sizeof(req.city), "%s", request_str + start_index);
 
-	// TODO: Configure server address
-	// struct sockaddr_in server_addr;
-	// ...
+	#if defined WIN32
+		WSADATA wsa_data;
+		int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
+		if (result != NO_ERROR) {
+			printf("Error at WSAStartup()\n");
+			return 0;
+		}
+	#endif
 
-	// TODO: Connect to server
-	// connect(...);
+		//creazione socket
+    int my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (my_socket < 0) {
+        errorhandler("creazione socket fallita");
+        clearwinsock();
+        return -1;
+    }
 
-	// TODO: Implement communication logic
-	// send(...);
-	// recv(...);
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(server_port);
 
-	// TODO: Close socket
-	// closesocket(my_socket);
+    //connessione
+    if (connect(my_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        errorhandler("connect() fallito");
+        closesocket(my_socket);
+        clearwinsock();
+        return -1;
+    }
 
-	printf("Client terminated.\n");
+    //invio dati al server
+    if (send(my_socket, &req, sizeof(req), 0) != sizeof(req)){
+        errorhandler("send() fallito");
+        closesocket(my_socket);
+        clearwinsock();
+        return -1;
+    }
 
-	clearwinsock();
-	return 0;
-} // main end
+    //ricezione dati dal server
+    weather_response_t resp;
+    memset(&resp, 0, sizeof(resp));
+
+    size_t total_received = 0;
+    size_t bytes_to_receive = sizeof(weather_response_t);
+
+    char *buffer = (char *)&resp;
+
+    while (total_received < bytes_to_receive) {
+        ssize_t bytes_rcvd = recv(my_socket, buffer + total_received, bytes_to_receive - total_received, 0);
+
+        if (bytes_rcvd == 0) {
+            printf("Connessione chiusa dal server prima di ricevere la struct completa.\n");
+            closesocket(my_socket);
+            clearwinsock();
+            return -1;
+        }
+        if (bytes_rcvd < 0) {
+            perror("recv() fallita");
+            closesocket(my_socket);
+            clearwinsock();
+            return -1;
+        }
+
+        total_received += bytes_rcvd;
+    }
+    resp.status = ntohl(resp.status);
+
+    //conversione del tipo
+    uint32_t raw_bytes;
+
+    memcpy(&raw_bytes, &resp.value, sizeof(resp.value));
+
+    raw_bytes = ntohl(raw_bytes);
+
+    memcpy(&resp.value, &raw_bytes, sizeof(resp.value));
+
+    //creazione messaggio di output
+    switch(resp.status){
+    case 0: if (req.type == 't') {
+    	printf("Ricevuto risultato dal server ip %s. %s: Temperatura = %.1f°C\n", server_ip, req.city, resp.value);
+    }
+    else if (req.type == 'h') {
+    	printf("Ricevuto risultato dal server ip %s. %s: Umidità = %.1f%%\n", server_ip, req.city, resp.value);
+    }
+    else if (req.type == 'w') {
+    	printf("Ricevuto risultato dal server ip %s. %s: Vento = %.1fkm/h\n", server_ip, req.city, resp.value);
+    }
+    else if (req.type == 'p') {
+    	printf("Ricevuto risultato dal server ip %s. %s: Pressione = %.1fhPa\n", server_ip, req.city, resp.value);
+    }
+    else {
+
+    }
+    break;
+    case 1: errorhandler("Città non valida");
+    break;
+    case 2: errorhandler("Richiesta non valida");
+    break;
+    }
+
+    //chiusura socket
+    closesocket(my_socket);
+    clearwinsock();
+
+    return 0;
+}
--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -9,15 +9,27 @@
 #define PROTOCOL_H_
 
 // Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
+#define SERVER_PORT 56700  // Server port (change if needed)
 #define BUFFER_SIZE 512    // Buffer size for messages
+#define QUEUE_SIZE 5
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+typedef struct {
+    unsigned int status;  // Response status code
+    char type;            // Echo of request type
+    float value;          // Weather data value
+} weather_response_t;
 
-/*
- * Example function to implement:
- * int connect_to_server(const char* server_address);
- */
+typedef struct {
+    char type;        // Weather data type: 't', 'h', 'w', 'p'
+    char city[64];    // City name (null-terminated string)
+} weather_request_t;
+
+float get_temperature(void);    // Range: -10.0 to 40.0 °C
+float get_humidity(void);       // Range: 20.0 to 100.0 %
+float get_wind(void);           // Range: 0.0 to 100.0 km/h
+float get_pressure(void);       // Range: 950.0 to 1050.0 hPa
+void errorhandler(char*);
+void conversione(weather_response_t *res);
+void strmMaiusc(char *s);
 
 #endif /* PROTOCOL_H_ */
